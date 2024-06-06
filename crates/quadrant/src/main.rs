c26cef45a3341bf0140ba7aa477fdaf9e67d50ce
Bregy Malpartida <bregy@minsky.cc>
2024-06-06 01:49:47 -0500

    feat: Add quadrant functionality to determine the quadrant of a point

--- crates/quadrant/src/main.rs
+++ crates/quadrant/src/main.rs
@@ -1,3 +1,51 @@
+enum QuadrantNum {
+    Center,
+    I,
+    II,
+    III,
+    IV,
+}
+
+trait Quadrant {
+    fn get_quadrant(self) -> QuadrantNum;
+}
+
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+impl Quadrant for Point {
+    fn get_quadrant(self) -> QuadrantNum {
+        if self.x > 0.0 && self.y >= 0.0 {
+            QuadrantNum::I
+        } else if self.x <= 0.0 && self.y > 0.0 {
+            QuadrantNum::II
+        } else if self.x < 0.0 && self.y <= 0.0 {
+            QuadrantNum::III
+        } else if self.x >= 0.0 && self.y < 0.0 {
+            QuadrantNum::IV
+        } else {
+            QuadrantNum::Center
+        }
+    }
+}
+
+impl Point {
+    fn show_quad(self) {
+        match self.get_quadrant() {
+            QuadrantNum::I => println!("Quadrant I"),
+            QuadrantNum::II => println!("Quadrant II"),
+            QuadrantNum::III => println!("Quadrant III"),
+            QuadrantNum::IV => println!("Quadrant IV"),
+            QuadrantNum::Center => println!("It's not in a quadrant"),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p = Point { x: 0.0, y: 1.0 };
+    // p.x = 13.0;
+
+    p.show_quad();
 }
