86edce50a4f92803e8ea67bee0ff767f0eba5be5
Bregy Malpartida <bregy@minsky.cc>
2024-06-06 10:54:14 -0500

    refactor: Improve readability and performance of quadrant calculation logic

--- crates/quadrant/src/main.rs
+++ crates/quadrant/src/main.rs
@@ -3,23 +3,33 @@ struct Point {
     y: f32,
 }
 
-impl Point {
-    fn show_quad(self) {
-        let phi = (self.y / self.x).atan().to_degrees();
-        let quad = (phi / 180.0).round() as i32 % 4;
+#[derive(Debug)]
+enum Quadrant {
+    I,
+    II,
+    III,
+    IV,
+    Center,
+}
 
-        match quad {
-            0 => println!("Quadrant I"),
-            1 => println!("Quadrant II"),
-            2 => println!("Quadrant III"),
-            3 => println!("Quadrant IV"),
-            _ => println!("It's the center!"),
+impl Point {
+    fn get_quad(self) -> Quadrant {
+        match (self.x.signum() as i32, self.y.signum() as i32) {
+            (1, 1) => Quadrant::I,
+            (-1, 1) => Quadrant::II,
+            (-1, -1) => Quadrant::III,
+            (1, -1) => Quadrant::IV,
+            (0, 0) => Quadrant::Center,
+            (1, 0) | (0, 1) => Quadrant::I,
+            (-1, 0) | (0, -1) => Quadrant::III,
+            _ => unreachable!(),
         }
     }
 }
 
 fn main() {
-    let p = Point { x: 1.0, y: -1.0 };
+    let p = Point { x: -1.0, y: -1.0 };
 
-    p.show_quad();
+    let q = p.get_quad();
+    println!("Quadrant {:?}", q);
 }
