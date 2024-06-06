e52aa46a2a36444510be60d89f37a87e027212a8
Bregy Malpartida <bregy@minsky.cc>
2024-06-06 02:30:25 -0500

    feat: refactor quadrant calculation logic to improve readability and performance

--- crates/quadrant/src/main.rs
+++ crates/quadrant/src/main.rs
@@ -1,51 +1,25 @@
-enum QuadrantNum {
-    Center,
-    I,
-    II,
-    III,
-    IV,
-}
-
-trait Quadrant {
-    fn get_quadrant(self) -> QuadrantNum;
-}
-
 struct Point {
     x: f32,
     y: f32,
 }
 
-impl Quadrant for Point {
-    fn get_quadrant(self) -> QuadrantNum {
-        if self.x > 0.0 && self.y >= 0.0 {
-            QuadrantNum::I
-        } else if self.x <= 0.0 && self.y > 0.0 {
-            QuadrantNum::II
-        } else if self.x < 0.0 && self.y <= 0.0 {
-            QuadrantNum::III
-        } else if self.x >= 0.0 && self.y < 0.0 {
-            QuadrantNum::IV
-        } else {
-            QuadrantNum::Center
-        }
-    }
-}
-
 impl Point {
     fn show_quad(self) {
-        match self.get_quadrant() {
-            QuadrantNum::I => println!("Quadrant I"),
-            QuadrantNum::II => println!("Quadrant II"),
-            QuadrantNum::III => println!("Quadrant III"),
-            QuadrantNum::IV => println!("Quadrant IV"),
-            QuadrantNum::Center => println!("It's not in a quadrant"),
+        let phi = (self.y / self.x).atan().to_degrees();
+        let quad = (phi / 180.0).round() as i32 % 4;
+
+        match quad {
+            0 => println!("Quadrant I"),
+            1 => println!("Quadrant II"),
+            2 => println!("Quadrant III"),
+            3 => println!("Quadrant IV"),
+            _ => println!("It's the center!"),
         }
     }
 }
 
 fn main() {
-    let p = Point { x: 0.0, y: 1.0 };
-    // p.x = 13.0;
+    let p = Point { x: 1.0, y: -1.0 };
 
     p.show_quad();
 }
